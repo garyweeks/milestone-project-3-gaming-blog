b02364a4f34ca96a14d7abc82de7c5000114a189
The-Gazzara <76946929+The-Gazzara@users.noreply.github.com>
2025-10-23 13:29:26 +0200

    created and linked database and created syuperuser for blog admin

--- project3gamingblog/settings.py
+++ project3gamingblog/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -51,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'codestar.urls'
+ROOT_URLCONF = 'project3gamingblog.urls'
 
 TEMPLATES = [
     {
@@ -69,17 +73,21 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mp3-gaming-blog.codestar.wsgi.application'
+WSGI_APPLICATION = 'project3gamingblog.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
