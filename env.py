1a665cd8d8d23f43404965522ed47f120ff3d2a2
The-Gazzara <76946929+The-Gazzara@users.noreply.github.com>
2025-10-24 08:34:49 +0200

    added post model

--- env.py
+++ env.py
@@ -2,4 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_6StfHQrnCbg2@ep-aged-violet-agzc445n.c-2.eu-central-1.aws.neon.tech/april_snub_sect_920149")
-os.environ.setdefault("SECRET_KEY", "X'51:^2^`r)~(Xz~Js,oag0}J`R#]F")
+os.environ.setdefault("SECRET_KEY", "X'51:^2^`r)~(Xz~Js,oag}J0`R#]F")
