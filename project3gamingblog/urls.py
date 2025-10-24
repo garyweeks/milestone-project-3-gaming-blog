732c87cdbe4844018b3ae517cc202795acec1c0a
The-Gazzara <76946929+The-Gazzara@users.noreply.github.com>
2025-10-24 12:10:00 +0200

    added django summertime

--- project3gamingblog/urls.py
+++ project3gamingblog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for project3gamingblog project.
+URL configuration for codestar project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
